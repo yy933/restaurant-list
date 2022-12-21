c57f646fce8c8eb91b0e695ad59e5d28f6752d7e
yy933 <emilyyu025@gmail.com>
2022-12-21 12:42:32 +0800

    feat: add CRUD function

---
+++ models/restaurants.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const restaurantSchema = new Schema({
+  name: { type: String, required: true },
+  name_en: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String },
+  location: { type: String },
+  phone: { type: String, required: true },
+  google_map: { type: String },
+  rating: { type: Number, required: true },
+  description: { type: String },
+});
+module.exports = mongoose.model("restaurants", restaurantSchema);
---
+++ models/seeds/restaurantsSeeder.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const restaurants = require("../restaurants"); // 載入 restaurant model
+const restaurantList = require("../../restaurantList.json").results
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+db.once("open", () => {
+  console.log("mongodb connected!");
+
+  restaurants.create(restaurantList)
+   .then(()=>{
+    console.log('restaurantSeeder done!')
+    db.close()
+   })
+   .catch(error=>console.log(error))
+});
