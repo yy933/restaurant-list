3247cbd5f5f10b498de543f9ad701026d999260a
yy933 <emilyyu025@gmail.com>
2023-02-12 20:30:53 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,10 @@ const passport = require('passport')
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/users/login");
+});
 router.get('/register', (req, res) => {
   res.render('register')
 })
